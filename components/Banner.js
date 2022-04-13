2be5b933de47bcda01e12034db7001ea79043569
Apurba Naskar <ElectronicAPU.apurbanaskar3206@gmail.com>
2022-04-13 13:25:02 +0530

    first commit

---
+++ components/Banner.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <>
+
+<div className="">
+      <div className="">
+        <div className="">
+          <div className="">
+            <h1>
+              Smart <br />
+              Consulting <br />
+              Group.
+            </h1>
+          </div>
+          <div className="">
+            <h2>
+              We are helping clint to create technology that uses <br />
+              Blockhain. AI and IOT with our talented expert.
+            </h2>
+          </div>
+          <div className="">
+            <h3>Consult with an expart now!</h3>
+          </div>
+          <div className="">
+            <h5 className="">Already a member?</h5>
+            <h5>
+              <a href=""> <span>Sign in.</span></a>
+            </h5>
+          </div>
+        </div>
+        <div className="">
+          <div className="">
+            <div className="">
+              <img className="" src="/img/Right.png" alt="" />
+            </div>
+            <div className="">
+              <img className="" src="/img/Image.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    </>
+  )
+}
+
+export default Banner
---
+++ components/WhatWeDo.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const WhatWeDo = () => {
+  return (
+    <>
+        <div className="flex justify-between px-12">
+     
+
+      <div className="w-6/12 px-4">
+        <div>
+          <img src="/Img/Icon.png" alt="" />
+        </div>
+        <div className="text-lg py-2">
+          <h1 className="font-serif font-semibold">Marketing.</h1>
+        </div>
+        <div>
+          <p className="text-xs">Creating a spacing how people move through.</p>
+        </div>
+      </div>
+
+      
+
+      <div className="w-6/12 px-4">
+        <div>
+          <img src="./Img/Icon.png" alt="" />
+        </div>
+        <div className="text-lg py-2">
+          <h1 className="font-serif font-semibold">Dev & Design.</h1>
+        </div>
+        <div>
+          <p className="text-xs">
+            Creating a higher spacing how people move through. From its mel
+            origins to the digital era.
+          </p>
+        </div>
+      </div>
+
+      
+
+      <div className="w-6/12 px-4">
+        <div>
+          <img src="./Img/5.png" alt="" />
+        </div>
+        <div className="text-lg py-2">
+          <h1 className="font-serif font-semibold">Creativity.</h1>
+        </div>
+        <div>
+          <p className="text-xs">
+            Get a full cRontrol of debts in the dsu digital world simplesity.
+            labore et dolore magna aliqua.
+          </p>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default WhatWeDo
