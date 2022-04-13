25b1732d4e562d546bd3ae287f597421eb8b300e
AMIYA8597 <amiyachowdhury04@.com>
2022-04-13 14:07:03 +0530

    where

--- components/WhatWeDo.js
+++ components/WhatWeDo.js
@@ -3,52 +3,54 @@ import React from 'react'
 const WhatWeDo = () => {
   return (
     <>
-        <div className="flex justify-between px-12">
-     
-
-      <div className="w-6/12 px-4">
-        <div>
-          <img src="/Img/Icon.png" alt="" />
+        <div className="bg-green-200 flex justify-center p-2">
+      <div
+        className="bg-red-200 flex flex-col md:flex-row md:justify-between sm:max-w-2xl lg:max-w-4xl"
+      >
+        <div
+          className="m-2 md:w-1/4 flex flex-col items-center text-center md:block md:text-left"
+        >
+          <div className="w-12">
+            <img src="/Img/Icon.png" alt="star icon" className="w-full" />
+          </div>
+          <div className="my-2 text-3xl">
+            <h1>Marketing.</h1>
+          </div>
+          <div>
+            <p>Creating a spacing how people move through.</p>
+          </div>
         </div>
-        <div className="text-lg py-2">
-          <h1 className="font-serif font-semibold">Marketing.</h1>
+        <div
+          className="m-2 md:w-1/4 flex flex-col items-center text-center md:block md:text-left"
+        >
+          <div className="w-12">
+            <img src="/Img/Icon.png" alt="star icon" className="w-full" />
+          </div>
+          <div className="my-2 text-3xl">
+            <h1>Dev & Design.</h1>
+          </div>
+          <div>
+            <p>
+              Creating a higher spacing how people move through. From its mel
+              origins to the digital era.
+            </p>
+          </div>
         </div>
-        <div>
-          <p className="text-xs">Creating a spacing how people move through.</p>
-        </div>
-      </div>
-
-      
-
-      <div className="w-6/12 px-4">
-        <div>
-          <img src="./Img/Icon.png" alt="" />
-        </div>
-        <div className="text-lg py-2">
-          <h1 className="font-serif font-semibold">Dev & Design.</h1>
-        </div>
-        <div>
-          <p className="text-xs">
-            Creating a higher spacing how people move through. From its mel
-            origins to the digital era.
-          </p>
-        </div>
-      </div>
-
-      
-
-      <div className="w-6/12 px-4">
-        <div>
-          <img src="./Img/5.png" alt="" />
-        </div>
-        <div className="text-lg py-2">
-          <h1 className="font-serif font-semibold">Creativity.</h1>
-        </div>
-        <div>
-          <p className="text-xs">
-            Get a full cRontrol of debts in the dsu digital world simplesity.
-            labore et dolore magna aliqua.
-          </p>
+        <div
+          className="m-2 md:w-1/4 flex flex-col items-center text-center md:block md:text-left"
+        >
+          <div className="w-12">
+            <img src="/Img/Icon.png" alt="star icon" className="w-full" />
+          </div>
+          <div className="my-2 text-3xl">
+            <h1>Creativity.</h1>
+          </div>
+          <div>
+            <p>
+              Get a full cRontrol of debts in the dsu digital world simplesity.
+              labore et dolore magna aliqua
+            </p>
+          </div>
         </div>
       </div>
     </div>
