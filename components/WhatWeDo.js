ae69a03eab6dbe9ccd9c90cbeeb42daafea923cf
ElectronicAPU <100857852+ElectronicAPU@users.noreply.github.com>
2022-04-19 20:04:21 +0530

    Hello

--- components/WhatWeDo.js
+++ components/WhatWeDo.js
@@ -3,53 +3,7 @@ import React from "react";
 const WhatWeDo = () => {
   return (
     <>
-<<<<<<< HEAD
-      <div className="flex flex-wrap justify-around px-12 mt-16 sm:flex-row md:px-28 lg:px-36 ">
-        <div className="px-4 sm: my-4">
-          <div className=" ">
-            <img className="w-8" src="/Img/Icon.png" alt="" />
-          </div>
-          <div className="text-lg py-2">
-            <h1 className="font-serif font-semibold">Marketing.</h1>
-          </div>
-          <div>
-            <p className="text-xs">
-              Creating a spacing how people <br />
-              move through.
-            </p>
-          </div>
-        </div>
-
-        <div className="px-4 sm: my-4">
-          <div>
-            <img className="w-8" src="/Img/Icon.png" alt="" />
-          </div>
-          <div className="text-lg py-2">
-            <h1 className="font-serif font-semibold">Dev & Design.</h1>
-          </div>
-          <div>
-            <p className="text-xs">
-              Creating a higher spacing how <br />
-              people move through. From its mel <br />
-              origins to the digital era.
-            </p>
-          </div>
-        </div>
-
-        <div className="px-4 sm: my-4">
-          <div>
-            <img className="w-8" src="/Img/5.png" alt="" />
-          </div>
-          <div className="text-lg py-2">
-            <h1 className="font-serif font-semibold">Creativity.</h1>
-          </div>
-          <div>
-            <p className="text-xs">
-              Get a full cRontrol of debts in the <br />
-              dsu digital world simplesity. labore <br />
-              et dolore magna aliqua.
-=======
-        <div className="bg-green-200 flex justify-center p-2">
+<div className="bg-green-200 flex justify-center p-2">
       <div
         className="bg-red-200 flex flex-col md:flex-row md:justify-between sm:max-w-2xl lg:max-w-4xl"
       >
@@ -95,16 +49,8 @@ const WhatWeDo = () => {
             <p>
               Get a full cRontrol of debts in the dsu digital world simplesity.
               labore et dolore magna aliqua
->>>>>>> b6af9e8cc01047b15df7f6f6fc34b1dcace90124
             </p>
           </div>
-        </div>
-      </div>
-<<<<<<< HEAD
-=======
-    </div>
->>>>>>> b6af9e8cc01047b15df7f6f6fc34b1dcace90124
-    </>
   );
 };
 
